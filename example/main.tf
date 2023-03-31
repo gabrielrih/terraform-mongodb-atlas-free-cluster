4a7bf8effb91e1a7b3ee2fd3e17272e059783e93
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2023-03-30 22:38:39 -0300

    Remote state on Terraform Cloud

--- example/main.tf
+++
@@ -1,8 +0,0 @@
-module "example" {
-  source              = "../"
-  project_id          = "62a91310a30761611b2c4e1a"
-  cluster_name        = "test-tf"
-  ip_address_to_allow = "10.10.10.10"
-  username            = "temp_user"
-  password            = "putYourPasswordHere"
-}
--- provider.tf
+++ provider.tf
@@ -5,5 +5,12 @@ terraform {
       version = "~> 1.3.1"
     }
   }
-  required_version = ">=1.2.2"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "ae2b1fca515949e5d54fb22b8ed95575"
+    workspaces {
+      name = "mongodb-atlas"
+    }
+  }
+  required_version = ">=1.4.4"
 }
